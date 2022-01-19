1983b0e687b63846944f2e38998f6fa1b7e6f5d4
shalini Kumarii <bittukumari@gmail.com>
2022-01-19 08:52:01 -0800

    Coin flipping first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  function tossCoin() {
+    return Math.random() > 0.5 ? "heads" : "tails";
+  }
+  function fiveHeads() {
+    return new Promise((resolve, reject) => {
+
+      // your code here!
+      let headsCount = 0;
+      let attempts = 0;
+      while (headsCount < 5) {
+        attempts++;
+        let result = tossCoin();
+        console.log(`${result} was flipped`);
+        if (result === "heads") {
+          headsCount++;
+        } else {
+          headsCount = 0;
+        }
+      }
+      return `It took ${attempts} tries to flip five "heads"`;
+});
+}
+fiveHeads()
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
+console.log("When does this run now?");
+          
+
+ 
+
+
+return (
+  <div className="App">
+
+  </div>
   );
 }
-
 export default App;
